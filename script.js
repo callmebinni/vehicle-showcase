27b11db2ea4eb61575de6416ff2c75033dd88548
callmebinni <rjdollar00@gmail.com>
2026-03-11 08:41:15 +0300

    Commit Changes

---
+++ script.js
@@ -0,0 +1,8 @@
+function activateMode(){
+
+alert("⚡ Hyper Mode Activated! Welcome to the future of vehicles.");
+
+document.body.style.background =
+"linear-gradient(45deg,#020617,#0f172a,#020617)";
+
+}
